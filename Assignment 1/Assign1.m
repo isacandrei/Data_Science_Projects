be9f6c763ee12af59c4bf5a6b5837decd8124893
david-pavlov <davidcho@abv.bg>
2016-11-17 13:24:46 +0100

    Assignment 1
    Movie data preprocess

---
+++ Assignment 1/Assign1.m
@@ -0,0 +1,12 @@
+M=readtable('movievalue.csv');
+%---------------------------------------
+M.ProductionBudget=strrep(M.ProductionBudget, '$', '');
+M.ProductionBudget{:}=str2double(M.ProductionBudget{:});
+%---------------------------------------
+M.DomesticGross=strrep(M.DomesticGross, '$', '');
+M.DomesticGross{:}=str2double(M.DomesticGross{:});
+%---------------------------------------
+M.WorldwideGross=strrep(M.WorldwideGross, '$', '');
+M.WorldwideGross{:}=str2double(M.WorldwideGross{:});
+%---------------------------------------
+M.ReleaseDate=datetime(M.ReleaseDate,'InputFormat','MM/dd/yyyy');
