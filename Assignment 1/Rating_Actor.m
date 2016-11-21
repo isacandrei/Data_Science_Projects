1544a274b761c1bceaca883664f15d6d14ee0367
WinLeongX <winlxuan@gmail.com>
2016-11-21 23:25:06 +0100

    Final Actor Rating code

--- Assignment 1/Rating_Actor.m
+++ Assignment 1/Rating_Actor.m
@@ -1,43 +1,71 @@
-Actor = 'Zoe Saldana';
-Avg_rating = 0;
-Other_rating = 0;
-C1 = 0;
-C2 = 0;
+% Generate Actor List
+y = [];
 
-% Find movies starring Actor
-App = strfind(M.Actors, Actor);
+for i=1:height(M)
+    z = strsplit(char(M.Actors{i}), ', ');
+    y = [y, z];
+end;
 
-% Convert cell to logical array
-App = ~cellfun(@isempty,App);
+y = unique(y);
+y = transpose(y);
 
-% Find indices of movies starring Actor
-i = find(App);
+% Initiliaze array to store average ratings
+list_avg = zeros(length(y),1);
+list_all = ones(length(y),1);
+All_avg = 0;
 
-% Compute Average Rating starring Actor
-for k=1:length(i)
-   
-    % Skip value if value is NaN
-    if isnan(M.imdbRating(i(k)))      
-    else
-        Avg_rating = Avg_rating + M.imdbRating(i(k));
-    	C1 = C1 + 1;
-   end
-end
+% Calculate Ratings for each Actor
+for q = 1:length(y)
+    
+    Avg_rating = 0;
+    % Other_rating = 0;
+    C1 = 0;
+    C2 = 0;
+    
+    % Find movies starring Actor
+    App = strfind(M.Actors, y(q));
 
-Avg_rating = Avg_rating / C1;
+    % Convert cell to logical array
+    App = ~cellfun(@isempty,App);
 
-% Find indices of the rest
-i_rest = find(not(App));
+    % Find indices of movies starring Actor
+    i = find(App);
 
-% Compute Average Rating of the rest
-for l=1:length(i_rest)
-   
-    % Skip value if value is NaN
-    if isnan(M.imdbRating(i_rest(l)))      
-    else
-        Other_rating = Other_rating + M.imdbRating(i_rest(l));
-    	C2 = C2 + 1;
+    % Compute Average Rating starring Actor
+    for k=1:length(i)
+
+        % Skip value if value is NaN
+        if isnan(M.imdbRating(i(k)))      
+        else
+            Avg_rating = Avg_rating + M.imdbRating(i(k));
+            C1 = C1 + 1;
+       end
+    end
+
+    list_avg(q) = Avg_rating / C1;
+    
+    % Filter out NaN values
+    if isnan(list_avg(q))
+        list_avg(q) = 0;
     end
 end
 
-Other_rating = Other_rating / C2;
+% Compute Average of whole list
+    for l=1:height(M)
+
+        % Skip value if value is NaN
+        if isnan(M.imdbRating(l))      
+        else
+            All_avg = All_avg + M.imdbRating((l));
+            C2 = C2 + 1;
+        end
+    end
+
+% List average rating next to Actor rating
+All_avg = All_avg / C2;  
+list_all = All_avg * list_all;
+
+% Create table with Ratings
+Rating_Table = table(y, list_avg, list_all, 'VariableNames',{'Actors','Starring','Average'});
+
+Rating_Table = sortrows(Rating_Table,-2);
