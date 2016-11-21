b17e7fa8353dd06be67bcfd3220db522f99005e2
isacandrei <isacandrei19@gmail.com>
2016-11-21 19:25:32 +0100

    bonus part

--- Assignment 1/Assign1.m
+++ Assignment 1/Assign1.m
@@ -1,5 +1,5 @@
 % Import CSV file into MatLab as table
-M = readtable('movievalue.csv');
+M = readtable('movievalue.csv', 'delimiter', ';');
 
 % Remove $ signs and convert values to doubles
 M.ProductionBudget = strrep(M.ProductionBudget, '$', '');
@@ -15,8 +15,8 @@ M.WorldwideGross = str2double(M.WorldwideGross);
 M.ReleaseDate = datetime(M.ReleaseDate,'InputFormat','MM/dd/yyyy');
 
 % Replace unknown characters
-M.Movie = strrep(M.Movie, 'â€”', '-');
-M.Movie = strrep(M.Movie, 'â€™', char(39));
+M.Movie = strrep(M.Movie, '???', '-');
+M.Movie = strrep(M.Movie, '???', char(39));
 
 % Get information from API and join them to table
 n=1200;
@@ -35,7 +35,7 @@ for i=1:length(y)
     arr(i,3)=length(moviesPerYear);
     for j=1:length(moviesPerYear)
         %calculate the total gross of all movies released that year
-        arr(i,2)=M.WorldwideGross(i);
+        arr(i,2)= arr(i,2) + M.WorldwideGross(moviesPerYear(j));
     end;
     %calculate the average
     arr(i,2)=arr(i,2)/arr(i,3);
@@ -48,4 +48,29 @@ bar(arr(:,1),arr(:,2));
 
 figure(4)
 plot(M.Rank(1:n),(M.WorldwideGross(1:n)));
+
+
+figure(9)
+T = M(1:n,:);
+x = unique(T.ProductionBudget);
+
+A=zeros(length(x),2);
+
+A(:,1) = x;
+
+for i=1:length(x)
+    moviesPerBudget = find(T.ProductionBudget == x(i));
+    l = length(moviesPerBudget);
+    for j = 1:l
+        %calculate the total gross of all movies released that year
+        A(i,2) = A(i,2) + T.WorldwideGross(moviesPerBudget(j)) - T.ProductionBudget(moviesPerBudget(j));
+    end;
+    %calculate the average
+    A(i,2) = A(i,2)/l;
+end;
+
+%[Y,I]=sort(A(1,:));
+%B=A(:,I); %use the column indices from sort() to sort all columns of A.
+
+bar(A(:,1), A(:,2));
 
