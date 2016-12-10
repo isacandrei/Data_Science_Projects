45b071250c4b2e8b18057fb7ac8775cc63a49323
isacandrei <isacandrei19@gmail.com>
2016-12-10 14:15:03 +0100

    create age distribution graph

--- Assignment_4/userProfileAnalytics.m
+++ Assignment_4/userProfileAnalytics.m
@@ -7,3 +7,21 @@ M = M(~(strcmp(M.sex, '') | isnan(M.age) | strcmp(M.country, '')), :);
 M = M(M.age > 7, :);
 
 M = M(M.age < 120, :);
+
+ageRanges = [0 18; 18 25; 25 35; 35 45; 45 60; 60 120];
+
+countAge = zeros(size(ageRanges,1),1);
+for i = 1:size(ageRanges,1)
+    countAge(i) = height(M(M.age > ageRanges(i,1) & M.age < ageRanges(i,2), :));
+end
+
+ageGroups = {'<18', '18-25', '25-35', '35-45', '45-60', '60+'};
+
+figure
+bar(countAge)
+set(gca, 'XTick', 1:size(ageGroups,2), 'XTickLabel', ageGroups);
+title('Age distribution')
+xlabel('Age Group')
+ylabel('Number of users')
+
+
