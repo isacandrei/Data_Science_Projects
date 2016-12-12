013ad31af2ed70c08f9d596427b00785464d1e8b
isacandrei <isacandrei19@gmail.com>
2016-12-13 00:23:07 +0100

    code comment + other cleanups + features

--- Assignment_4/genes.m
+++ Assignment_4/genes.m
@@ -1,13 +1,13 @@
+%read + clean data
 G = readtable('genes.csv', 'delimiter', '\t');
 G(1,:) = [];
-gene = G(:,1); 
+gene = G(:,2); 
 G(:,[1,2,3]) = [];
 clones = G.Properties.VariableNames;
 
+%generate matrix for both underexpressed and overexpressed genes (same matrix)
 noGenes = height(G);
 matrix = zeros(2*noGenes,300);
-
-
 
 tic; %Measure time needed to run code
 
@@ -25,7 +25,7 @@ time = toc
 
 matrix = transpose(matrix);
 
-
+%setup algorithm parameters
 minSup = 0.01;
 minConf = 0.2;
 nRules = 1000;
@@ -33,37 +33,48 @@ sortFlag = 1;
 fname = 'GenesRules'; 
 
 for s = 1:noGenes
-    labels{s} = ['Gene ' gene.YORF{s} ' high'];
+    labels{s} = ['Gene ' gene.NAME{s} ' high'];
 end
 for s = 1:noGenes
-    labels{s+noGenes} = ['Gene ' gene.YORF{s} ' low'];
+    labels{s+noGenes} = ['Gene ' gene.NAME{s} ' low'];
 end
 
-% tic; %Measure time needed to run code
-% 
-% [Rules, FreqItemsets, RuleConf, RuleSup] = findRules(matrix, minSup, minConf, nRules, sortFlag, labels, fname);
-% 
-% time = toc
-% 
-% noUp = 0;
-% for i = 1:size(RuleConf,1)
-%     if(Rules{1}{i} > noGenes || Rules{2}{i} > noGenes)
-%         noUp = noUp + 1;
-%     end
-% end
+tic; %Measure time needed to run code
+
+%run algorithm
+[Rules, FreqItemsets, RuleConf, RuleSup] = findRules(matrix, minSup, minConf, nRules, sortFlag, labels, fname);
+
+time = toc
+
+noUp = 0;
+for i = 1:size(RuleConf,1)
+    if(Rules{1}{i} > noGenes || Rules{2}{i} > noGenes)
+        noUp = noUp + 1;
+    end
+end
 
 %consider only the overexpressed genes
 newMatrix = matrix(:, (1:noGenes));
 labelsUp = labels(:,(1:noGenes));
 
-minSup = 0.15;
+%setup algorithm parameters
+
+minSup = 0.1;
 minConf = 0.8;
 nRules = 10000;
 sortFlag = 1;
-fname = 'GenesRulesUp2'; 
+fname = 'GenesRulesUpNames'; 
 
 tic; %Measure time needed to run code
+%run algorithm
 
 [RulesUp, FreqItemsetsUp, RuleConfUp, RuleSupUp] = findRules(newMatrix, minSup, minConf, nRules, sortFlag, labelsUp, fname);
 
 time = toc
+
+%generate over/undexexpressed genes frequent item sets
+V = FreqItemsets(:,3);
+V = vertcat(V{:});
+M = array2table(V);
+M.Properties.VariableNames = {'a' 'b' 'c'};
+
