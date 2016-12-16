b33c4be906d752ea6ecd91579653bdbf4cffdc0a
david-pavlov <davidcho@abv.bg>
2016-12-16 11:43:21 +0100

    6d

--- Assignment_5/Assign1.m
+++ Assignment_5/Assign1.m
@@ -1,8 +1,13 @@
-data = readtable('start_3d.txt', 'delimiter', ' ');
+data = readtable('stars_6d.txt', 'delimiter', ' ');
 
 data = table2array(data);
+data = datasample(data,0.1*length(data));
 
-c = clusterdata(data, 'linkage', 'ward','savememory', 'on', 'maxclust', 4);
-
+c = clusterdata(data, 'linkage', 'ward','savememory', 'on', 'maxclust', 40);
+hold on;
+for i=1:length(unique(c))
+    idx=find(c==i);
+    quiver3(data(idx,1),data(idx,2),data(idx,3),data(idx,4),data(idx,5),data(idx,6))
+end;
 figure(1)
 scatter3(c(:,1), c(:,2), c(:,3));
