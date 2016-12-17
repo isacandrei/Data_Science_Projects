67cbe618c477e105cb6eeb5d0f425b3a370ca6f7
david-pavlov <davidcho@abv.bg>
2016-12-17 18:01:41 +0100

    hierarchical clustering

--- Assignment_5/Assign1.m
+++ Assignment_5/Assign1.m
@@ -2,12 +2,13 @@ data = readtable('stars_6d.txt', 'delimiter', ' ');
 
 data = table2array(data);
 data = datasample(data,0.1*length(data));
-
-c = clusterdata(data, 'linkage', 'ward','savememory', 'on', 'maxclust', 40);
+c
+c = clusterdata(data, 'linkage', 'ward','savememory', 'on', 'maxclust', 5);
 hold on;
-for i=1:length(unique(c))
-    idx=find(c==i);
-    quiver3(data(idx,1),data(idx,2),data(idx,3),data(idx,4),data(idx,5),data(idx,6))
+for i=1:length(unique(cl))
+    idx=find(cl==i);
+scatter3(data(idx,1),data(idx,2),data(idx,3));
+%quiver3(data(idx,1),data(idx,2),data(idx,3),data(idx,4),data(idx,5),data(idx,6))
 end;
 figure(1)
 scatter3(c(:,1), c(:,2), c(:,3));
