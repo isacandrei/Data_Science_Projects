9c8f02a3fec733c4ca5665046c7470a5b5556672
WinLeongX <winlxuan@gmail.com>
2016-12-20 17:22:26 +0100

    placed images in folder

--- Assignment_5/assign2win.m
+++ Assignment_5/assign2win.m
@@ -1,7 +1,9 @@
-data = readtable('start_3d.txt', 'delimiter', ' ');
+data = readtable('stars_3d.txt', 'delimiter', ' ');
 
 data = table2array(data);
 
 data = datasample(data, 49500);
 
 eva = evalclusters(data,'kmeans','Silhouette','Klist',[1:50]);
+
+wss = wss(data, 50);
