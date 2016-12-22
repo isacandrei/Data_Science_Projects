04b1d9890f46d209a8daeedda607c66b52aa2d50
WinLeongX <winlxuan@gmail.com>
2016-12-22 12:23:06 +0100

    Update Assign3_3.m

--- Assignment_5/Assign3_3.m
+++ Assignment_5/Assign3_3.m
@@ -10,6 +10,7 @@ GMModel = fitgmdist(data3d, 35, 'Start', 'plus', 'RegularizationValue', 0.1);
 scatter3(data3d(:,1),data3d(:,2),data3d(:,3),10,P(:,1));
 hb = colorbar;
 
+x=P>0.7;
 a=sum(x,1);
 max_cluster=find(a==max(a));
 q = find(P(:,max_cluster)>0.7);
@@ -30,6 +31,7 @@ GMModel = fitgmdist(data6d, 50, 'Start', 'plus', 'RegularizationValue', 0.1);
 quiver3(data6d(:,1),data6d(:,2),data6d(:,3),data6d(:,4),data6d(:,5),data6d(:,6),10,P(:,1));
 hb = colorbar;
 
+x=P>0.7;
 a=sum(x,1);
 max_cluster=find(a==max(a));
 q = find(P(:,max_cluster)>0.7);
