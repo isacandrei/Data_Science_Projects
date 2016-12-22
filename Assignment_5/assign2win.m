dc475082a28ff957746d8ed0273408b7120b9ab2
WinLeongX <winlxuan@gmail.com>
2016-12-22 02:01:08 +0100

    assign 3 code and images

--- Assignment_5/assign2win.m
+++
@@ -1,9 +0,0 @@
-data = readtable('stars_3d.txt', 'delimiter', ' ');
-
-data = table2array(data);
-
-data = datasample(data, 49500);
-
-eva = evalclusters(data,'kmeans','Silhouette','Klist',[1:50]);
-
-wss = wss(data, 50);
